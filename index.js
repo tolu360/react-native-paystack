f5347d0a1cb23567ad028917cd664331edbb7124
Tolu Olowu <o.tolulope@gmail.com>
2017-06-09 15:49:13 +0100

    Retire getToken and add chargeCardWithAccessCode on iOS

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ import { NativeModules, Platform } from 'react-native'
 const RNPaystackNative = NativeModules.RNPaystack
 
 class RNPaystack {
-	getToken(cardParams) {
-		if (typeof cardParams != 'object') {
-			return Promise.reject(new Error("Method argument can only be a Javascript object"));
-		}
-
-		return RNPaystackNative.getToken(cardParams);
-	}
-
 	chargeCard(chargeParams) {
 		if (typeof chargeParams != 'object') {
 			return Promise.reject(new Error("Method argument can only be a Javascript object"));
 		}
 
 		return RNPaystackNative.chargeCard(chargeParams);
+	}
+
+	chargeCardWithAccessCode(chargeParams) {
+		if (typeof chargeParams != 'object') {
+			return Promise.reject(new Error("Method argument can only be a Javascript object"));
+		}
+
+		return RNPaystackNative.chargeCardWithAccessCode(chargeParams);
 	}
 }
 
